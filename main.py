de2696bbb0a8e090e220a90a7958310678324f5f
Arghya-Tech99 <sinhaarghya03@gmail.com>
2025-10-07 11:29:38 +0530

    Enabled keyboard controls for the player
    Enabled keyboard controls in the X direction

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ screen = pygame.display.set_mode((800, 600)) # (width pixels, height pixels) - f
 
 '''
 Event in PyGame - any action or message that the operating system or the user generates, which Pygame can detect and respond to.
+Events are generally keyboard inputs or mouse clicks, in the context of game development.
 pygame.event.get() -  used to retrieve and remove event messages from the Pygame event queue.
 
 When you retrieve events using pygame.event.get(), you get a list of pygame.event.Event objects. 
@@ -26,19 +27,45 @@ pygame.display.set_icon(icon)
 playerIcon = pygame.image.load('Player.png')
 playerX = 380
 playerY = 480
+playerX_change = 0
+playerY_change = 0
 
-def player(): # Function which draws the icon at the initial position defined on the game window
-    screen.blit(playerIcon, (playerX, playerY))
+def player(x, y): # Function which draws the icon at the initial position defined on the game window
+    screen.blit(playerIcon, (x, y))
 
 
 # Making the Game Loop
 running = True
 while running:
     screen.fill((0, 0, 0))  # Fill the game window with background color in RGB format
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        # Checks whether a keystroke is made or not, and if made then is it right or left
+        if event.type == pygame.KEYDOWN:
+            print('Keystroke detected')
+            if event.key == pygame.K_LEFT:
+                print('LEFT')
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                print('RIGHT')
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            print('Keystroke released')
+            playerX_change = 0
+           
+
+
+    '''
+    pygame.KEYDOWN - Function which checks whether a key is pressed.
+    pygame.KEYUP - Function which checks whether a key is released.
+    '''
+
+    # Update the values of playerX and playerY
+    playerX += playerX_change
+
+    # Changing the X and Y coordinates of the player, and drawing it repeatedly on the screen by calling the function
+    player(playerX, playerY)
+
     pygame.display.update() # Updates the game window
